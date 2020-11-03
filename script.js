a8fdfae98f54dc523014797da0dd0d1de173e3a4
flotr18 <55090943+flotr18@users.noreply.github.com>
2020-11-03 16:53:23 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,9 @@ const favouriteColor = '#152228'; // Navy Blue
 const favouriteFood = 'Hamburger';
 const city = 'London';
 
+console.log(firstName);
+console.log(favouriteColor);
+console.log(favouriteFood);
+console.log(city);
 
+// Display in the console the value of firstName, favouriteColor, favouriteFood, city
