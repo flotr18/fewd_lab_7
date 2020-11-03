5e33a7704e00a663b8db9397645e80418f1707d8
flotr18 <55090943+flotr18@users.noreply.github.com>
2020-11-03 16:57:35 +0000

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ console.log(favouriteFood);
 console.log(city);
 
 // Display in the console the value of firstName, favouriteColor, favouriteFood, city
+
+const phrase = 'A rainbow is formed by the sunlight. Shining through rain drops so small. ' +
+    'The rain bends the light from the sunshine, and colors the sky for us all.';
+const endingPhrase = 'is what we see. Rainbow colours, are made with light bending you see!';
+const one = 'Red';
+const two = 'orange';
+const three = 'yellow';
+const four = 'green';
+const five = 'blue';
+const six = 'indigo';
+const seven = 'violet';
+
+
